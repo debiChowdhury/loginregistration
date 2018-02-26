9edcb34b6b34fe25a4d3ee2a68c771d04b0f669d
Debi Chowdhury <debi.chowdhury@learningmatekol.local>
2018-02-26 18:47:56 +0530

    activity component

--- src/app/act1/act1.component.ts
+++ src/app/act1/act1.component.ts
@@ -8,16 +8,38 @@ import { RouterModule, Router } from '@angular/router';
   styleUrls: ['./act1.component.css']
 })
 export class Act1Component implements OnInit {
-  bookList: Array<any> = [];
+  bookList: any[] = [];
+  libraryType: string;
+  visibleBookList: any[] = [];
+  libraryOption = [];
   constructor(private _booklist: BookListService, private _router: Router) { }
-
   ngOnInit() {
-    this._booklist.getBookList().subscribe(data => {
+    this._booklist.getBookList().subscribe((data) => {
       this.bookList = data;
       console.log(this.bookList);
+      this.getBook();
+      this.libraryOption=Object.keys(this.bookList);
     });
   }
-
+  getlibraryOption() {
+    this.visibleBookList = [];
+    console.log(this.libraryType);
+    for (let key in this.bookList) {
+      if (this.libraryType === key) {
+        this.bookList[`${key}`].map((item) => {
+          this.visibleBookList.push(item);
+        });
+      }
+      };
+    };
+  getBook() {
+    for (let key in this.bookList) {
+      this.bookList[`${key}`].map((item) => {
+        this.visibleBookList.push(item);
+      });
+    };
+  }
+  //route to the particular books
   routeToBook(id) {
     this._router.navigate(['/activity1/book', { id: id }]);
 
